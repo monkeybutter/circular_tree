f81680067a588d93ec0bec4347e3c85126edfa33
monkeybutter <p.rozas.larraondo@gmail.com>
2015-12-07 20:47:38 +1100

    test helper functions

--- util.py
+++ util.py
@@ -1,9 +1,54 @@
 import numpy as np
+import pandas as pd
+from random import random, randint
 from data.classic_data import ClassicData
 from data.subset_data import SubsetData
 from node.node import Node
 
 
+def bound_checker(bounds):
+    
+    prev = (None, None)
+    
+    for bound in bounds:
+        if not bound[0] < bound[1]:
+            return False
+        if None not in bound and prev[1] >= bound[0]:     
+            return False
+            
+        prev = bound
+        
+    return True
+
+
+def randomizer(i0, i1, prob):
+    if random() < prob and i0 < i1-1:
+        new_i0 = randint(i0, i1-1)
+        new_i1 = randint(new_i0+1, i1)
+        return (new_i0, new_i1)
+    else:
+        return None
+    
+    
+def create_simple_data(size):
+    input_a = np.sort(np.random.uniform(low=0.0, high=1000.0, size=size))
+    
+    i = 0
+    class_var = np.random.normal(i, .1, size=size)
+    
+    a, b = 0, size-1
+    
+    while True:
+        i += 1
+        bounds = randomizer(a, b, .8)
+        if bounds is None:
+            break
+        class_var[a:b] = np.random.normal(i, .1, size=b-a)
+        a, b = bounds
+    
+    return pd.DataFrame(np.vstack((input_a, class_var)).T, columns=['input1', 'class_var'])
+
+    
 def tree_pprinter(node):
     fmtr = ""
 
